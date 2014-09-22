a3e20d31d39cfd21df92162150fd5d6123cdb832
Tom Kaczocha <tom@rawphp.org>
2014-09-22 21:03:22 +1000

    Updated YamlException with new base RawException namespace.

--- lib/YamlException.php
+++ lib/YamlException.php
@@ -35,7 +35,7 @@
 
 namespace RawPHP\RawYaml;
 
-use RawPHP\RawBase\RawException;
+use RawPHP\RawBase\Exceptions\RawException;
 
 /**
  * Exception thrown when there are problems with parsing or dumping yaml.
